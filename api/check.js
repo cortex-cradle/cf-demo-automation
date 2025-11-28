cd357b1ba72f2a569232550d375c327b06a5001b
Cortex-Cradle <adrian73737b@gmail.com>
2025-11-28 12:39:39 +0530

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,24 +1,6 @@
-import { readFileSync } from "fs";
-import path from "path";
+import { getPaymentStatus } from "./webhook";
 
 export default function handler(req, res) {
-  const orderId = req.query.id;
-
-  if (!orderId) {
-    return res.status(400).json({ paid: false });
-  }
-
-  try {
-    const filePath = path.join(process.cwd(), "payment.json");
-    const data = JSON.parse(readFileSync(filePath, "utf8"));
-
-    if (data[orderId] === "PAID") {
-      return res.status(200).json({ paid: true });
-    } else {
-      return res.status(200).json({ paid: false });
-    }
-
-  } catch (err) {
-    return res.status(200).json({ paid: false });
-  }
+  const status = getPaymentStatus();
+  res.status(200).json(status);
 }
